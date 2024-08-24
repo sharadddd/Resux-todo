6131a7fedeb97b5d606c3b8999cc24cbd9e75fa2
Sharad Pandey <sharad868788@gmail.com>
2024-08-24 23:08:20 +0530

    todo application

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Welcome to Redux</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const initialState = {
+    todos: [{ id: 1, text: "Hello world" }]
+}
+
+
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        updateTodo: (state, action) => {
+            const { id, newText } = action.payload;
+            const todo = state.todos.find(todo => todo.id === id);
+            if (todo) {
+                todo.text = newText;
+            }
+        }
+    }
+});
+
+export const { addTodo, removeTodo } = todoSlice.actions
+
+export default todoSlice.reducer
